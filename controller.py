825ea83ccee1cfc90dedb637d3d4b1023b98eb31
FominaCathy <99419802+FominaCathy@users.noreply.github.com>
2022-06-19 22:25:26 +0400

    Revert "Отрефакторил контроллер"

--- controller.py
+++ controller.py
@@ -3,7 +3,6 @@ import database_module
 import logger
 
 
-
 def run():
     
     while True:
@@ -11,49 +10,45 @@ def run():
         command = interface.start_page()
 
         match command:
-            case '1':     # Список всех контактов
-                data = database_module.get_all_contacts()
-                interface.show_contacts(data)
+            case '1':     # Список всех дел
+                data = database_module.get_all_deals() ## прошу дописать функцию в data_base
+                interface.show_deals(data) ## предлагаю не вводить команду юзера, а после вывода дел вызывать главное меню + дописать исключение, если 'baseIsEmpty'
 
-            case '2': # Поиск контакта
-                user_search = interface.search_contact()
-                data = database_module.get_contact_info(user_search)
-                interface.show_contacts(data)
+            case '2': # Список дел по статусу
+                status = interface.show_deals_by_status()
+                while status not in range(1,6):
+                    interface.error_input()
+                    status = interface.show_deals_by_status()
+                data = database_module.get_status_deal(status) ## прошу переименовать функцию get_data_undone в data_base
+                interface.show_deals(data)
             
             
-            case '3': # Добавить контакт
+            case '3': # Добавить дело
 
-                new_contact = interface.add_contact()
-                database_module.add_contacts(new_contact)
-                logger.add(new_contact, 'added')
+                new_deal = interface.add_deal() # user_data - введенные юзером данные в удобном формате. Здесь я их преобразую в стандартный наш словарь и передам в data_base и logger
+                database_module.add_deal(new_deal)
+                logger.add(new_deal, 'added')
                 interface.done_message()
 
             case '4': # Изменить дело
-                data = database_module.get_all_contacts()
-                interface.show_contacts(data)
-                deal_id = interface.change_contact()
-                one_contact = database_module.get_one_contact(deal_id)
-                changed_contact = interface.change_contact_content(one_contact)
-                if changed_contact['comment'] == 'Я что-то нажал и всё сломалось':
-                    database_module.delete_contact(changed_contact['contact_id'])
-                    logger.add(changed_contact, 'deleted')
+                data = database_module.get_all_deals()
+                interface.show_deals(data)
+                deal_id = interface.change_deal()
+                one_deal = database_module.get_one_deal(deal_id)
+                changed_deal = interface.change_deal_content(one_deal)
+                if changed_deal['status'] == 10:
+                    database_module.delete_deal(changed_deal['deal_id'])
+                    logger.add(changed_deal, 'deleted')
                 else:
-                    database_module.change_contact(changed_contact)
-                    logger.add(changed_contact, 'changed')
+                    database_module.change_deal(changed_deal)
+                    logger.add(changed_deal, 'changed')
             
-            case '5': # Импорт
-                print('Функционал в процессе разработки')
-            
-            
-            case '6': # Экспорт
-                print('Функционал в процессе разработки')
-
-            case '7': # Выход
-                interface.bye_mess()
+            case '5': # Выход
+                interface.bye_mess() # прошу дописать функцию bye в interface (прощание с юзером)
                 break
             
             case _:
-                interface.error_input()
+                interface.error_input() # прошу дописать функцию error_input в interface, которая выведет сообщение что-то вроде "Введены некорректные данные"
 
 
 def change_action(user_answer: dict):

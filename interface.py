db6640bd875dd734c1d593860bf55ed56aa1d858
SergeiPospelov <pospelka413@gmail.com>
2022-06-19 17:28:37 +0300

    Определены основные функции

--- interface.py
+++ interface.py
@@ -1,19 +1,20 @@
 import time
-
-# import os
+from import_from_file import choice_format
 
 main_menu = \
     'Выберите пункт меню:\n\
-    1. \033[4mСписок всех дел\033[0m\n\
-    2. \033[4mСписок дел по статусу\033[0m\n\
-    3. \033[4mДобавить дело\033[0m\n\
-    4. \033[4mИзменить дело\033[0m\n\
-    5. \033[4mВыход\033[0m'
+    1. \033[4mСписок контактов\033[0m\n\
+    2. \033[4mПоиск контакта\033[0m\n\
+    3. \033[4mДобавить контакт вручную\033[0m\n\
+    4. \033[4mИзменить контакт\033[0m\n\
+    5. \033[4mИмпорт контактов\033[0m\n\
+    6. \033[4mЭкспорт контактов\033[0m\n\
+    7. \033[4mВыход из приложения\033[0m'
 
 
 def start_page():  # Starting page, choose number
     print('╔' + 48 * "=" + '╗')
-    print('            \033[3;36mСписок Дел v0.1\033[0m')
+    print('            \033[3;36mТелефонный справочник v0.1\033[0m')
     print('╚' + 48 * "=" + '╝')
     print(main_menu)
     print(50 * "=")
@@ -23,63 +24,57 @@ def start_page():  # Starting page, choose number
     return command
 
 
-def show_deals(data):  # 1 in menu
-
+def show_contacts(data):  # 1 in menu
     if data != []:
-
-        print('\033[4mСписок всех дел:\033[0m')
+        print('\033[4mСписок контактов:\033[0m')
         for item in range(len(data)):
-            a = data[item]['deal_id']
-            b = data[item]['deal']
-            c = data[item]['deadline']
-            d = data[item]['status']
-            print(f'{a}) {b}. {c}. {d}. ')
+            a = data[item]['contact_id']
+            b = data[item]['surname']
+            c = data[item]['name']
+            d = data[item]['phone']
+            e = data[item]['comment']
+            print(f'{a}) {b}. {c}. {d}. {e}.')
         print(50 * "•")
     else:
-        print('\033[33mСписок дел пуст\033[0m')
+        print('\033[33mСправочник пуст\033[0m')
 
 
-def show_deals_by_status():
-    print('\033[4mВыберите статус дела для отображения:\033[0m')
-    status = int(input('1-Новые, 2-В работе, 3-Отложенные, 4-Просроченные, 5-Выполенные'))
+def search_contact():
+    search_request = input('Введите данные для поиска: ')
     print(50 * "=")
-    return status
+    return search_request
 
 
-def finished_deals():
-    print('\033[4mСписок законченных дел:\033[0m')
-    #print(data)
-    print(50 * "=")
-    start_page()
-
-
-def add_deal():
-    print('\033[3mДобавление дела\033[0m')
+def add_contact():
+    print('\033[3mДобавление контакта\033[0m')
     print(50 * "-")
-    deal_name = input('Что необходимо сделать: ')  # plain text
-    deal_deadline = input('Укажите сроки выполнения в формате ДД-ММ-ГГ: ')  # DD-MM-YY
-    deal = {'deal_id': -1, 'deal': deal_name, 'deadline': deal_deadline, 'status': 1}
-    deal['status'] = 1
-    return deal
+    contact_surname = input('Введите фамилию ')  # plain text
+    contact_name = input('Введите имя ')  # DD-MM-YY
+    contact_number = input('Введите номер телефона')
+    commentary = input('Комментарий')
+    contact = [{'contact_id': '', 'surname': contact_surname, 'name': contact_name, 'phone': contact_number, 'comment': commentary}, ]
+    return contact  # возвращение списка словаря
 
 
-def change_deal():
-    print('\033[4mИзменение дела:\033[0m')
+def change_contact():
+    print('\033[4mИзменить контакт:\033[0m')
     print(50 * "~")
-    deal_id = input('Выберите номер дела которого необходимо изменить:')
-    return int(deal_id)
+    contact_id = input('Выберите контакт для внесения изменений:')
+    return int(contact_id)
 
 
-def change_deal_content(one_deal):
-    command = input('Что необходимо сделать:\n 1 - Изменить статус \n 2 - Изменить содержание \n 3 - Удалить дело')
+def change_contact_content(one_contact):
+    command = input('Что необходимо сделать: 1 - Изменить содержание \n 2 - Удалить контакт')#подменю для выбора  полей контакта
     if command == '1':
-        status = (input('1-Новые, 2-В работе, 3-Отложенные, 4-Просроченные, 5-Выполенные'))
-        one_deal['status'] = int(status)
+        contact_surname = input('Введите фамилию ')  # plain text
+        contact_name = input('Введите имя ')  # DD-MM-YY
+        contact_number = input('Введите номер телефона')
+        commentary = input('Комментарий')
+        one_contact = {'contact_id': '', 'surname': contact_surname, 'name': contact_name, 'phone': contact_number,
+                        'comment': commentary}
     elif command == '2':
-        one_deal['deal'] = (input('Новое описание дела: '))
-    elif command == '3':
-        one_deal['status'] = 10 #Статус дела на удаление
-    return one_deal
+        one_contact['comment'] = 'Я что-то нажал и всё сломалось'  # удаление по ID
+    return one_contact
 
 
 def bye_mess():  # 6 in menu
@@ -96,3 +91,11 @@ def error_input():
 def done_message():
     print('Выполнено!')
 
+
+def import_contacts(import_list):
+    print('\033[4mИмпорт контактов:\033[0m')
+    choice_format()
+    return
+
+def export_contacts():
+    return
